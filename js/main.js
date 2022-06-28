8fec08f36882f069e7cb5bff8d3c12412f81c215
Andrea Prina <and.prina@gmail.com>
2022-06-28 15:18:38 +0200

    M0 - Creato array con oggetti "team member"

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const teamMembers = [
+    {
+        nameAndSurname : "Wayne Barnett",
+        role : "Founder & CEO",
+        image : "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        nameAndSurname : "Angela Caroll",
+        role : "Chief Editor",
+        image : "angela-caroll-chief-editor.jpg",
+    },
+    {
+        nameAndSurname : "Walter Gordon",
+        role : "Office Manager",
+        image : "walter-gordon-office-manager.jpg",
+    },
+    {
+        nameAndSurname : "Angela Lopez",
+        role : "Social Media Manager",
+        image : "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nameAndSurname : "Scott Estrada",
+        role : "Developer",
+        image : "scott-estrada-developer.jpg",
+    },
+    {
+        nameAndSurname : "Barbara Ramos",
+        role : "Graphic Designer",
+        image : "barbara-ramos-graphic-designer.jpg",
+    }
+]
+
+console.log(teamMembers);
